68d3d5edf67084be2764af2feba4526099002d31
akhil mantha <akhilmantha@live.com>
2017-05-08 00:07:50 +0530

    Initial Commit

---
+++ Tip calculatorTests/Tip_calculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tip_calculatorTests.swift
+//  Tip calculatorTests
+//
+//  Created by akhil mantha on 08/05/17.
+//  Copyright © 2017 akhil mantha. All rights reserved.
+//
+
+import XCTest
+@testable import Tip_calculator
+
+class Tip_calculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
